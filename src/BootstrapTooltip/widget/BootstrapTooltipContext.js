49cd38fe4bc5e1799d69da86785c0d938a4bb55c
Ivo Sturm <ivo.sturm@firstconsulting.nl>
2018-01-30 13:39:47 +0100

    Update for spaces in script

--- src/BootstrapTooltip/widget/BootstrapTooltipContext.js
+++ src/BootstrapTooltip/widget/BootstrapTooltipContext.js
@@ -9,9 +9,9 @@ define([
 
         update: function (obj, callback) {
 			
-			this._contextObj = obj;
-			// 20180126 - Ivo Sturm - Added subscriptions to cater for listening to dataview refresh
-			this._resetSubscriptions();
+	    this._contextObj = obj;
+	    // 20180126 - Ivo Sturm - Added subscriptions to cater for listening to dataview refresh
+	    this._resetSubscriptions();
 			
             logger.debug(this.id + ".update");
 
@@ -30,30 +30,29 @@ define([
 
             callback();
         },
-		_resetSubscriptions: function () {
+	_resetSubscriptions: function () {
 
             if (this._contextObj && this._handles.length == 0) {
 
                 var contextHandle = mx.data.subscribe({
                     guid: this._contextObj.getGuid(),
                     callback: lang.hitch(this, function (guid) {
-						var guid = this._contextObj ? this._contextObj.getGuid() : null;
-						if (this.tooltipMessageMicroflow !== "") {
-							this._execMf(this.tooltipMessageMicroflow, guid, lang.hitch(this, function (string) {
-								this._tooltipText = string;
-								this._initializeTooltip();
-							}));
-						} else if (this.tooltipMessageString !== "") {
-							this._tooltipText = this.tooltipMessageString;
-							this._initializeTooltip();
-						} else {
-							this._initializeTooltip();
-						}
+			var guid = this._contextObj ? this._contextObj.getGuid() : null;
+			if (this.tooltipMessageMicroflow !== "") {
+				this._execMf(this.tooltipMessageMicroflow, guid, lang.hitch(this, function (string) {
+					this._tooltipText = string;
+					this._initializeTooltip();
+				}));
+			} else if (this.tooltipMessageString !== "") {
+				this._tooltipText = this.tooltipMessageString;
+				this._initializeTooltip();
+			} else {
+				this._initializeTooltip();
+			}
                     })
                 });
-				this._handles.push(contextHandle);
+		this._handles.push(contextHandle);
             }
-
         }
     });
 });
