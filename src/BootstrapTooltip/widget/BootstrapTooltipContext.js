96b2d3c6b318af04ed702648b26c0c427b81f70f
Ivo Sturm <ivo.sturm@firstconsulting.nl>
2018-01-30 13:38:29 +0100

    Added support for updating tooltiptext
    Fix for listening to updates/changes on the context entity.

--- src/BootstrapTooltip/widget/BootstrapTooltipContext.js
+++ src/BootstrapTooltip/widget/BootstrapTooltipContext.js
@@ -8,6 +8,11 @@ define([
     return declare("BootstrapTooltip.widget.BootstrapTooltipContext", [_bootstrapTooltipWidget], {
 
         update: function (obj, callback) {
+			
+			this._contextObj = obj;
+			// 20180126 - Ivo Sturm - Added subscriptions to cater for listening to dataview refresh
+			this._resetSubscriptions();
+			
             logger.debug(this.id + ".update");
 
             var guid = obj ? obj.getGuid() : null;
@@ -16,14 +21,39 @@ define([
                     this._tooltipText = string;
                     this._initializeTooltip();
                 }));
+            } else if (this.tooltipMessageString !== "") {
+                this._tooltipText = this.tooltipMessageString;
+                this._initializeTooltip();
             } else {
-                if (this.tooltipMessageString !== "") {
-                    this._tooltipText = this.tooltipMessageString;
-                }
                 this._initializeTooltip();
             }
 
             callback();
+        },
+		_resetSubscriptions: function () {
+
+            if (this._contextObj && this._handles.length == 0) {
+
+                var contextHandle = mx.data.subscribe({
+                    guid: this._contextObj.getGuid(),
+                    callback: lang.hitch(this, function (guid) {
+						var guid = this._contextObj ? this._contextObj.getGuid() : null;
+						if (this.tooltipMessageMicroflow !== "") {
+							this._execMf(this.tooltipMessageMicroflow, guid, lang.hitch(this, function (string) {
+								this._tooltipText = string;
+								this._initializeTooltip();
+							}));
+						} else if (this.tooltipMessageString !== "") {
+							this._tooltipText = this.tooltipMessageString;
+							this._initializeTooltip();
+						} else {
+							this._initializeTooltip();
+						}
+                    })
+                });
+				this._handles.push(contextHandle);
+            }
+
         }
     });
 });
